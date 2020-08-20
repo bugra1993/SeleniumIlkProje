9f228329654106a088dea314bc04020e53dfc704
bugra <bgrcmldg@gmail.com>
2020-08-21 03:33:17 +0800

    "selenium linktest version 0.1"

--- src/com/techproed/Day04_LinkText.java
+++ src/com/techproed/Day04_LinkText.java
@@ -7,23 +7,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class Day04_LocatersGiris1 {
+public class Day04_LinkText {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/bugracamlidag/Documents/selenium libraries/drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
         driver.get("http://a.testaddressbook.com/");
-        WebElement signInLink = driver.findElement(By.id("sign-in"));
-        signInLink.click();
-        WebElement emailBox = driver.findElement(By.className("form-control"));
-        emailBox.sendKeys("testtechproed@gmail.com");
-        WebElement passwordBox;
-        passwordBox = driver.findElement(By.id("session_password"));
-        passwordBox.sendKeys("Test1234!");
-        WebElement signInButton = driver.findElement(By.name("commit"));
-        signInButton.click();
 
+        WebElement signInLink = driver.findElement(By.linkText("Sign in"));
+        signInLink.click();
+
+        WebElement homeLink;
+        homeLink = driver.findElement(By.partialLinkText("Home"));
+        homeLink.click();
 
     }
 }
---
+++ src/com/techproed/Day04_LocatersTagName.java
@@ -0,0 +1,7 @@
+package com.techproed;
+
+public class Day04_LocatersTagName {
+    public static void main(String[] args) {
+
+    }
+}
