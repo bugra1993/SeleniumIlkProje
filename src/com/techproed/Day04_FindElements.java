b567bc44b815eea3aa0487ab45eeb53e84321412
bugra <bgrcmldg@gmail.com>
2020-08-21 04:10:07 +0800

    "selenium findelements version 0.1"

--- src/com/techproed/Day04_FindElements.java
+++ src/com/techproed/Day04_FindElements.java
@@ -5,23 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class Day04_LocatersTagName {
+public class Day04_FindElements {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/bugracamlidag/Documents/selenium libraries/drivers/chromedriver");
+
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
         driver.get("http://a.testaddressbook.com/");
 
-        WebElement homeLink= driver.findElement(By.tagName("a"));
-        String str=homeLink.getText();
-        System.out.println(str);
-
-
-
-
+        List<WebElement> elementsList = driver.findElements(By.tagName("a"));
+        for(WebElement w:elementsList){
+            System.out.println(w.getText());
+        }
 
     }
 }
--- src/com/techproed/GoogleSearchTest.java
+++ src/com/techproed/GoogleSearchTest.java
@@ -1,26 +1,18 @@
 package com.techproed;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class Day04_LocatersTagName {
+public class GoogleSearchTest {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/bugracamlidag/Documents/selenium libraries/drivers/chromedriver");
+
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
-        driver.get("http://a.testaddressbook.com/");
-
-        WebElement homeLink= driver.findElement(By.tagName("a"));
-        String str=homeLink.getText();
-        System.out.println(str);
-
-
-
+        driver.get("https://www.google.com/");
 
 
     }
