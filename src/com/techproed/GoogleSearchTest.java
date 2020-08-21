b2e5c3b271e751769bedbb82d4fa0e8ca42b0487
bugra <bgrcmldg@gmail.com>
2020-08-21 15:05:28 +0800

    "selenium findelements new version "

--- src/com/techproed/GoogleSearchTest.java
+++ src/com/techproed/GoogleSearchTest.java
@@ -1,6 +1,8 @@
 package com.techproed;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -13,6 +15,17 @@ public class GoogleSearchTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
         driver.get("https://www.google.com/");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("city bike");
+        searchBox.submit();
+        //WebElement searchButton = driver.findElement(By.name("btnK"));
+        //searchButton.click();
+        WebElement resultState = driver.findElement(By.id("result-stats"));
+        System.out.println(resultState.getText());
+        WebElement shopping = driver.findElement(By.partialLinkText("Shopping"));
+        shopping.click();
+
+        driver.quit();
 
 
     }
